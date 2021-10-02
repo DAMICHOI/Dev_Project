c5bbbd06f074ebd4c30d2dd1449f10ae0bde944c
eunryeong1 <68606265+eunryeong1@users.noreply.github.com>
2021-10-03 02:42:43 +0900

    Update App.vue

--- App.vue
+++ App.vue
@@ -2,42 +2,58 @@
   <body>
     <div class="row1">
       <div class="search">
-        <input type="text" placeholder="검색어 입력" />
+        <input type="text" placeholder="검색어 입력" class="search_input"/>
         <button>검색</button>
       </div>
     </div>
-
     <div class="row2">
       <ul class="top-menu">
-        <li><a class="menus" href="">Home</a></li>
-        <li><a class="menus" href="">Company</a></li>
-        <li><a class="menus" href="">Mypage</a></li>
-        <li><a class="menus" href="">Logout</a></li>
+        <li class="menu_li"><a class="menus" href="">Home</a></li>
+        <li class="menu_li"><a class="menus" href="">Company</a></li>
+        <li class="menu_li"><a class="menus" href="">Mypage</a></li>
+        <li class="menu_li"><a class="menus" href="">Logout</a></li>
       </ul>
     </div>
     <div class="row3">
-    <div id="Card_News">
-      <div class="Card_Container">
-        <div class="card card-0"></div>
-        <div class="card card-1"></div>
-        <div class="card card-2"></div>
-
-        <div class="card card-3"></div>
-        <div class="card card-4"></div>
-        <div class="card card-5"></div>
+      <div id="Card_News">
+        <div class="Card_Container">
+          <div class="card card-0">
+          </div>
+        </div>
       </div>
-    </div>
+      <div class="join">
+        <div class="join_id">
+                <h4>ID</h4>
+                <input type="email" name="" id="" placeholder="ID">
+            </div>
+            <div class="join_email">
+                <h4>email</h4>
+                <input type="email" name="" id="" placeholder="email">
+            </div>
+            <div class="join_pw">
+                <h4>Password</h4>
+                <input type="password" name="" id="" placeholder="Password">
+            </div>
+            
+            <div class="submit">
+                <input type="submit" value="sign in">
+            </div>
+            <div class="join_sns">
+            <li class="sns_li"><a href="">인스타<i class="fab fa-instagram"></i></a></li>
+            <li class="sns_li"><a href="">페북<i class="fab fa-facebook-f"></i></a></li>
+            <li class="sns_li"><a href="">트위터<i class="fab fa-twitter"></i></a></li>
+            </div>
+      </div>
+        
     </div>
   </body>
 </template>
-
 <script>
 export default {
   name: "DEV",
   components: {},
 };
 </script>
-
 <style>
 body {
   width: 100%;
@@ -61,7 +77,7 @@ body {
   margin: auto;
   border: 1px solid;
 }
-input {
+.search_input {
   font-size: 16px;
   width: 630px;
   padding: 10px;
@@ -83,9 +99,6 @@ button {
   align-items: center;
   background-color: white;
 }
-.row3{
-  overflow: auto;
-}
 @media screen and (max-width: 768) {
   .row2 {
     flex-direction: column;
@@ -93,7 +106,7 @@ button {
   .top-menu {
     flex-direction: column;
   }
-  .li {
+  .menu_li {
     padding: 0px 30px;
   }
 }
@@ -113,7 +126,7 @@ ul {
   margin: 5px auto;
   padding: 0;
 }
-li {
+.menu_li {
   text-decoration: none;
   color: black;
   padding-right: 60px;
@@ -122,22 +135,30 @@ li {
   font-weight: bold;
   display: inline;
 }
+.row3{
+  flex:1;
+  flex-shrink: 1;
+  overflow: auto;
+  display:flex;
+  margin:0 15%;
+}
 #Card_News {
   height: 80vh;
-  background: whitesmoke;
   text-align: center;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  flex:1;
 }
 .card {
   background: #fff;
   border-radius: 4px;
   display: inline-block;
-  height: 300px;
+  height: 450px;
   margin: 30px;
-  width: 300px;
+  width: 370px;
   border: 1px solid;
+  flex-shrink: 1;
 }
 .card-1 {
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
@@ -146,24 +167,111 @@ li {
 .card-0:hover {
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
-.card-1:hover {
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-}
-.card-2:hover {
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-}
-.card-3:hover {
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-}
-.card-4:hover {
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-}
-.card-5:hover {
-  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-}
 .Card_Container {
-  display: grid;
   grid-template-rows: auto;
   grid-template-columns: 1fr 1fr 1fr;
+}
+.join{
+  display:flex;
+  flex:1;
+  width:auto;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+h4{
+  margin:3px;
+}
+h2 {
+  color: tomato;
+  font-size: 2em;
+}
+.join_sns {
+  
+  padding: 20px;
+  display: flex;
+}
+.join_sns li {
+  padding: 0px 20px;
+  list-style-type: none;
+
+
+}
+.join_sns a {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border-radius: 50px;
+  background: white;
+  font-size: 15px;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.4), -3px -3px 5px rgba(0, 0, 0, 0.1);
+}
+.join_id {
+  margin-top: 20px;
+  width:80%;
+  }
+.join_email {
+  margin-top: 20px;
+  width:80%;
+  }
+.join_email input {
+  width: 100%;
+  height: 50px;
+  border-radius: 30px;
+  margin-top: 10px;
+  padding: 0px 20px;
+  border: 1px solid lightgray;
+  outline: none;
+}
+  
+.join_id input {
+  width: 100%;
+  height: 50px;
+  border-radius: 30px;
+  margin-top: 10px;
+  padding: 0px 20px;
+  border: 1px solid lightgray;
+  outline: none;
+}
+.join_pw {
+  margin-top: 20px;
+  width: 80%;
+}
+.join_pw input {
+  width: 100%;
+  height: 50px;
+  border-radius: 30px;
+  margin-top: 10px;
+  padding: 0px 20px;
+  border: 1px solid lightgray;
+  outline: none;
+}
+
+.submit {
+  margin-top: 10px;
+  width: 80%;
+}
+.submit input {
+  width: 100%;
+  height: 50px;
+  margin:8px 0px;
+  border: 0;
+  outline: none;
+  border-radius: 40px;
+  background:yellowgreen; 
+  color: white;
+  font-size: 1.2em;
+  letter-spacing: 2px;
+}
+.join_members{
+  flex:0.4;
+}
+
+.join_member{
+  height:40px;
+  width:100px;
 }
 </style>
